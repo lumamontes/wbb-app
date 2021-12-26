30d8a077fd33301fafe9ddbd96045af2e5f09779
Luma Montes <lumagoesmontes@gmail.com>
2021-12-26 05:36:09 -0300

    ajustado exibiçao de dados

--- src/pages/Rankings/index.js
+++ src/pages/Rankings/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Image, TouchableOpacity, Button, FlatList } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import TextBold from '../../../src/components/TextBold'
 import TextBoldBlack from '../../../src/components/TextBoldBlack'
 import RankCard from '../../components/RankCard'
-
+import api from '../../services/api';
 export default function Rankings() {
+  const [rankings, setRankings] = useState({});
+
+  const LoadRankings = async () => {    // try{
+    try {
+      const response = await api.get(`polls/US/2021/rankings.json?api_key=x4c6asbawqk5zw9uswgm3aqh`);
+      const teste = await response.data;
+      const { year, week, effective_time, rankings } = teste;
+      setRankings(rankings);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    LoadRankings();
+  }, [])
+
   return (
     <View style={styles.container}>
       <TextBold size={18}>USA Today Coaches Poll</TextBold>
-
       <View style={styles.rankingsContainer}>
         <View style={styles.firstContent}>
           <TextBoldBlack>1
@@ -21,31 +36,28 @@ export default function Rankings() {
               uri: 'https://imgur.com/VDMSnH8.jpg',
             }}
           />
-          <TextBoldBlack>South Carolina</TextBoldBlack>
-          <TextBoldBlack>73 votes</TextBoldBlack>
+          <TextBoldBlack>{rankings[0].market} </TextBoldBlack>
+          <TextBoldBlack>{rankings[0].fp_votes} Votes</TextBoldBlack>
         </View>
 
         <View>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="2"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="3"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="4"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="5" />
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="6"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="7"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="8"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="9"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="10"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="11"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="12"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="13"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="14"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="15"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="6"/>
-          <RankCard team="South Carolina" back="#D1D1D1" votes="13" rank="6"/>
-          <RankCard team="South Carolina" back="#D8D8D8" votes="13" rank="6"/>
+          <FlatList
+            data={rankings.slice(1, 25)}
+            keyExtractor={ranking => String(ranking.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item: ranking }) => 
+            (
+              <View>
+              <RankCard team={ranking.market} back="#D8D8D8" votes={ranking.fp_votes} rank={ranking.rank} />
+             </View>
+            )}
+          >
+
+
+          </FlatList>
         </View>
       </View>
-    </View>
+    </View >
   );
 }
 
@@ -59,7 +71,7 @@ const styles = StyleSheet.create({
   rankingsContainer: {
     flex: 1,
     alignItems: 'center',
-    
+
     backgroundColor: '#D1D1D1',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
